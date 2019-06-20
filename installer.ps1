b92ae239cbadc2b95c3716cdb88080502a91a60a
LE POMMELET <william.le-pommelet@capgemini.com>
2019-06-20 19:05:34 +0200

    launchy .ini update and scanning

--- installer.ps1
+++ installer.ps1
@@ -3,8 +3,20 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 # Packages' installation using chocolatey
 foreach($package in Get-Content .\package.txt) {
-    choco install $package
+    choco install $package -y
 }
 
 # Ungit's installation using npm (choclatey repo isn't up to date sadly)
 npm install -g ungit
+
+# Replace launchy.init with home made config
+Copy-Item -Path "launchy.ini" -Destination "$env:AppData/Launchy/launchy.ini" -force
+if ($?) {
+    Write-Host "Launchy's .ini config updated"
+} else {
+    Write-Host "Error while updating Launchy's .ini config"
+    Write-Host "Maybe worth checking privileges' settings or doing it by hand (refer to https://github.com/William-LP/CAC/blob/master/launchy.ini)"
+}
+
+# Rescanning the disk using the new .ini file
+& "${env:ProgramFiles(x86)}\Launchy\Launchy.exe" /rescan
