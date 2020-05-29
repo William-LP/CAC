cfc1419a0fd77e4c19e94481acec2c65d893700e
William-LP <william.lepommelet@gmail.com>
2020-05-29 04:13:23 +0200

    remove download func + remove nom mode as not used

--- main.go
+++ main.go
@@ -9,38 +9,10 @@ import (
 	"os/exec"
 )
 
-func DownloadFile(filepath string, url string) error {
-
-    // Get the data
-    resp, err := http.Get(url)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-
-    // Create the file
-    out, err := os.Create(filepath)
-    if err != nil {
-        return err
-    }
-    defer out.Close()
-
-    // Write the body to file
-    _, err = io.Copy(out, resp.Body)
-    return err
-}
-
-
 // Install : allows to install package
-func Install(mode, pkg string) {
+func Install(pkg string) {
 	var cmd *exec.Cmd
-	switch(mode) {
-	case "choco" :
-		cmd = exec.Command("choco", "install", pkg, "-y")
-	case "npm" :
-		cmd = exec.Command("npm", "install", "-g", pkg)
-	}
-
+	cmd = exec.Command("choco", "install", pkg, "-y")
 	stdout, err := cmd.Output()
 
 	if err != nil {
@@ -60,17 +32,11 @@ func main() {
 		chocoPackage := scanner.Text()
 		log.Println("install :",chocoPackage)
 
-		Install("choco", chocoPackage)
+		Install(chocoPackage)
 
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
 	}
-	Install("npm", "ungit")
 
-	fileURL := "https://golangcode.com/images/avatar.jpg"
-
-    if err := DownloadFile("avatar.jpg", fileURL); err != nil {
-        panic(err)
-    }
 }
